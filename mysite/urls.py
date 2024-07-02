ac5acf4f57d8cea7a809c5152f29bd52116878f7
icehongssii12345 <icehongssii@gmail.com>
2024-07-02 22:59:11 +0900

    Feat: init polls 어플리케이션

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from polls import views as polls_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', polls_views.index, name='index'),
 ]
