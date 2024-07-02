c9005405d50f48eb4a92441590ba233983f5e42a
icehongssii12345 <icehongssii@gmail.com>
2024-07-02 23:05:50 +0900

    Feat: polls 모델 init

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Questions(models.Model):
+    pass 
+    # 질문
+    question_text = models.CharField(max_length=200)
+    # 질문 생성 시각
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text    
+    
+    
+class Choices(models.Model):
+    pass
+    # pk 
+    # 초이스내용
+    choice_text = models.CharField(max_length=200)
+    # 질문 id 
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    # 투표수
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    
