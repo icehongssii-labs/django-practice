aae46db60c4bd4fbb9497a4313a974f34a79fe7f
icehongssii12345 <icehongssii@gmail.com>
2024-07-03 07:53:12 +0900

    Fix: admin 등록 오타 수정

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Questions, Choices
+from .models import Questions, Choices
 # Register your models here.
-admin.register(Questions)
-admin.register(Choices)
+
+admin.site.register(Questions)
+admin.site.register(Choices)
