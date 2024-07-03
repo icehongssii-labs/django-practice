c9e1ba16fdd41b6bc86c127de75ab8ed814b46b6
icehongssii12345 <icehongssii@gmail.com>
2024-07-03 09:41:11 +0900

    Feat: 질문, 사지선다 뷰 템플릿

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from .models import Questions, Choices
+
 
 def index(request):
-    return JsonResponse( {"hi":"hi"})
+    latest_question_list = Questions.objects.all().order_by("-pub_date")[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def results(request, question_id:int):
     return JsonResponse( {f"question_id {question_id}":"results"})
 
 def detail(request, question_id:int):
-    return JsonResponse( {f"question_id {question_id}":"detail"})
+    question = get_object_or_404(Questions, pk=question_id)    
+    return render(request, 'polls/detail.html', {'question':question})
 
 def vote(reqest, question_id:int):
     return JsonResponse( {f"question_id {question_id}":"vote"})
